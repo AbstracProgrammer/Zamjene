8d7c04c10ec9861ec98652418fb52360842ddad6
AbstracProgrammer <98614971+AbstracProgrammer@users.noreply.github.com>
2025-02-16 21:02:04 +0100

    listati kroz profesore ali trebao bih definirati fiksnu duljinu za ime profesore, jer koliko vidim malo je naporno stalno micati mis

--- raspored/scripts/changeTeachers.js
+++ raspored/scripts/changeTeachers.js
@@ -18,8 +18,54 @@ function setCurrentTeacher(name) {
   display.textContent = name;
 }
 
+//gumbove za sljedeći, prijašnji prof
+const previousTeacherButton = document.querySelector("#left");
+const nextTeacherButton = document.querySelector("#right");
+
+function previousTeacher() {
+  const pointer = absentTeachers.indexOf(currentTeacher);
+  if (pointer == 0) {
+    return;
+  } else if (pointer == 1) {
+    previousTeacherButton?.classList.add("forbidden-cycle");
+    previousTeacherButton?.removeEventListener("click", previousTeacher);
+  }
+
+  if (pointer + 1 == absentTeachers.length) {
+    nextTeacherButton?.classList.remove("forbidden-cycle");
+    nextTeacherButton?.addEventListener("click", nextTeacher);
+  }
+  currentTeacher = absentTeachers[pointer - 1];
+  setCurrentTeacher(currentTeacher);
+}
+
+function nextTeacher() {
+  const pointer = absentTeachers.indexOf(currentTeacher);
+  if (pointer + 1 == absentTeachers.length) {
+    return;
+  } else if (pointer + 2 == absentTeachers.length) {
+    nextTeacherButton?.classList.add("forbidden-cycle");
+    nextTeacherButton?.removeEventListener("click", nextTeacher);
+  }
+
+  if (pointer == 0) {
+    previousTeacherButton?.classList.remove("forbidden-cycle");
+    previousTeacherButton?.addEventListener("click", previousTeacher);
+  }
+
+  currentTeacher = absentTeachers[pointer + 1];
+  setCurrentTeacher(currentTeacher);
+}
+
 //ODMAH generirati prvog prof
 setCurrentTeacher(absentTeachers[0]);
+let currentTeacher = absentTeachers[0]; //mozda ovdje export
+previousTeacherButton?.classList.add("forbidden-cycle");
+if (absentTeachers.length == 1) {
+  nextTeacherButton?.classList.add("forbidden-cycle");
+} else {
+  nextTeacherButton?.addEventListener("click", nextTeacher);
+}
 
 /**
  * IDEJA ZA OVE KLIKOVE
