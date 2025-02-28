a80c8ef37763a16639fb39e75498e81a63827d5a
AbstracProgrammer <98614971+AbstracProgrammer@users.noreply.github.com>
2025-02-28 19:10:12 +0100

    dodao biranje posebnog elemenate moram dodati i prazan sat

--- odrediZamjenu/scripts/substitution.js
+++ odrediZamjenu/scripts/substitution.js
@@ -1,45 +1,65 @@
+function markAsSelected(element) {
+  checkIfThereIsSelectedItem(element.parentElement);
+  if (element.classList.contains("element-selected")) {
+    element.classList.remove("element-selected");
+    return;
+  }
+  element.classList.add("element-selected");
+}
+
+function checkIfThereIsSelectedItem(parentElement) {
+  const selectedItem = parentElement.querySelector(".element-selected");
+  if (selectedItem == null) {
+    return;
+  }
+  selectedItem.click();
+}
+
+function makeListItem(text) {
+  const itemElement = document.createElement("li");
+  itemElement.classList.add("element");
+  itemElement.textContent = text;
+  return itemElement;
+}
+
 export function fillTeachersList(sortedTeachersLists) {
   const teacherListElement = document.querySelector("#teacher-list");
 
   for (let i = 0; i < sortedTeachersLists[0].length; i++) {
-    const itemElement = document.createElement("li");
-    itemElement.classList.add("element");
     const bestTeacher = sortedTeachersLists[0][i];
+    const itemElement = makeListItem(bestTeacher);
     itemElement.classList.add("best");
-    itemElement.textContent = bestTeacher;
     teacherListElement?.appendChild(itemElement);
+    itemElement.addEventListener("click", () => markAsSelected(itemElement));
   }
   for (let i = 0; i < sortedTeachersLists[1].length; i++) {
-    const itemElement = document.createElement("li");
-    itemElement.classList.add("element");
     const goodTeacher = sortedTeachersLists[1][i];
+    const itemElement = makeListItem(goodTeacher);
     itemElement.classList.add("good");
-    itemElement.textContent = goodTeacher;
     teacherListElement?.appendChild(itemElement);
+    itemElement.addEventListener("click", () => markAsSelected(itemElement));
   }
   for (let i = 0; i < sortedTeachersLists[2].length; i++) {
-    const itemElement = document.createElement("li");
-    itemElement.classList.add("element");
     const badTeacher = sortedTeachersLists[2][i];
+    const itemElement = makeListItem(badTeacher);
     itemElement.classList.add("bad");
-    itemElement.textContent = badTeacher;
     teacherListElement?.appendChild(itemElement);
+    itemElement.addEventListener("click", () => markAsSelected(itemElement));
   }
 }
 
 export function fillClassroomList(classesList) {
-  const teacherListElement = document.querySelector("#room-list");
+  const classroomListElement = document.querySelector("#room-list");
 
   for (let i = 0; i < classesList.length; i++) {
-    const itemElement = document.createElement("li");
-    itemElement.classList.add("element");
     const className = classesList[i];
+    const itemElement = makeListItem(className);
     if (i == 0) {
       itemElement.classList.add("best");
     } else {
       itemElement.classList.add("bad");
     }
-    itemElement.textContent = className;
-    teacherListElement?.appendChild(itemElement);
+    classroomListElement?.appendChild(itemElement);
+    itemElement.addEventListener("click", () => markAsSelected(itemElement));
   }
 }
