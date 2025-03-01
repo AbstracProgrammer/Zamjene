16fbf083d94501d5c47da9a9be540dbf75d8cf84
AbstracProgrammer <98614971+AbstracProgrammer@users.noreply.github.com>
2025-03-01 17:10:44 +0100

    djelomicno mijenjanje profesora ne radi u potpunosti jer jos moram ograniciti ako nije spremljeno sve itd.

--- odrediZamjenu/scripts/index.js
+++ odrediZamjenu/scripts/index.js
@@ -25,8 +25,9 @@ let listCurrentAbsenceText,
   goodTeachersList,
   badTeachersList,
   currentTeacher,
+  currentTeacherIndex,
   currentIndexAbsence,
-  currentTeacherTotal;
+  currentPeriodsTotal;
 
 async function setUpStartingScreen() {
   [listCurrentAbsenceText, listCurrentAbsenceJSON] =
@@ -36,8 +37,9 @@ async function setUpStartingScreen() {
   currentAbsence = listCurrentAbsenceJSON[0];
   currentAbsenceText = listCurrentAbsenceText[0];
   currentTeacher = absentTeachers[0];
+  currentTeacherIndex = 0;
   currentIndexAbsence = 0;
-  currentTeacherTotal = defineTotal(false, listCurrentAbsenceText);
+  currentPeriodsTotal = defineTotal(false, listCurrentAbsenceText);
 
   console.log(currentAbsence);
   [bestTeachersList, goodTeachersList, badTeachersList] = await sortTeachers(
@@ -57,6 +59,12 @@ async function setUpStartingScreen() {
     nextDayPeriodButton?.classList.add("forbidden-cycle");
   } else {
     nextDayPeriodButton?.addEventListener("click", nextDayPeriod);
+  }
+  previousTeacherButton?.classList.add("forbidden-cycle");
+  if (listCurrentAbsenceText.length == 1) {
+    nextTeacherButton?.classList.add("forbidden-cycle");
+  } else {
+    nextTeacherButton?.addEventListener("click", nextTeacher);
   }
   modal.style.display = "none";
 }
@@ -105,4 +113,52 @@ function nextDayPeriod() {
   generateNewDayPeriod(currentAbsence, currentTeacher);
 }
 
+const previousTeacherButton = document.querySelector("#left-teacher");
+const nextTeacherButton = document.querySelector("#right-teacher");
+
+async function updateInformationWhenChaningTeacher() {
+  currentTeacher = absentTeachers[currentTeacherIndex];
+  [listCurrentAbsenceText, listCurrentAbsenceJSON] =
+    await extractSelectedClasses(currentTeacher);
+
+  currentIndexAbsence = 0;
+  currentAbsenceText = listCurrentAbsenceText[currentIndexAbsence];
+  currentAbsence = listCurrentAbsenceJSON[currentIndexAbsence];
+  changeCurrentDisplay(true, currentTeacher);
+  changeCurrentDisplay(false, currentAbsenceText);
+  generateNewDayPeriod(currentAbsence, currentTeacher);
+  currentPeriodsTotal = defineTotal(false, listCurrentAbsenceText);
+  displaySavedSubstitutions(listCurrentAbsenceJSON);
+}
+
+async function previousTeacher() {
+  //funkcija ako nije spremljeno
+  if (currentTeacherIndex == 1) {
+    previousTeacherButton?.classList.add("forbidden-cycle");
+    previousTeacherButton?.removeEventListener("click", previousTeacher);
+  }
+
+  if (currentTeacherIndex + 1 == absentTeachers.length) {
+    nextTeacherButton?.classList.remove("forbidden-cycle");
+    nextTeacherButton?.addEventListener("click", nextTeacher);
+  }
+  currentTeacherIndex--;
+  await updateInformationWhenChaningTeacher();
+  console.log(listCurrentAbsenceText, listCurrentAbsenceJSON);
+}
+
+async function nextTeacher() {
+  //funkcija ako nije spremljeno
+  if (currentTeacherIndex + 2 == absentTeachers.length) {
+    nextTeacherButton?.classList.add("forbidden-cycle");
+    nextTeacherButton?.removeEventListener("click", nextTeacher);
+  }
+  if (currentTeacherIndex == 0) {
+    previousTeacherButton?.classList.remove("forbidden-cycle");
+    previousTeacherButton?.addEventListener("click", previousTeacher);
+  }
+  currentTeacherIndex++;
+  await updateInformationWhenChaningTeacher();
+  console.log("nakon", listCurrentAbsenceText, listCurrentAbsenceJSON);
+}
 setUpStartingScreen();
